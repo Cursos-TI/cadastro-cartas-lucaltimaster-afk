160606d0dce8b9076c4b08255f6c1dc81574882a
lucaltimaster-afk <lucaltimaster@gmail.com>
2026-05-14 20:16:11 +0000

    SPTrunfo Final

--- SPTrunfoD.c
+++ SPTrunfoD.c
@@ -5,15 +5,15 @@
 int main(){
 
     //Declaração das variaveis.
-    char estado[4], cod[5], cidade[50];
-    int populacao, Pturi;
+    char estado[2], cod[5], cidade[50];
+    int populacao, Pturi, x = 0;
     float area, Dpop, PIBpc;
-    double pib;
+    double pib, super, semiSp;
 
-    char estado2[4], cod2[5], cidade2[50];
-    int populacao2, Pturi2;
+    char estado2[2], cod2[5], cidade2[50];
+    int populacao2, Pturi2, y = 0;
     float area2, Dpop2, PIBpc2;
-    double pib2;
+    double pib2, super2, semiSp2;
 
     //Coleta de dados inseridos pelo usuário.
     printf("Carta 1:\n");
@@ -27,11 +27,13 @@ int main(){
     if (estado[0] >= 'A' && estado[0] <= 'H')
     {
 
-    } else {
+    }
+    else 
+    {
         printf("Inválido\n");
         return 0;
     }
-
+    
     //Consome o \n deixado pelo scanf
     getchar();
 
@@ -41,6 +43,17 @@ int main(){
     printf("Insira o codigo (Estado + número de 01 a 04): ");
     scanf("%s", cod);
 
+    //Converte letras minúsculas para maiúsculas.
+    cod[0] = toupper(cod[0]);
+
+    //Compara se a primeira letra do cod é igual ao estado.
+    if (cod[0] != estado[0])
+    {
+        printf("Inválido");
+        return 0;
+    }
+    
+    //Coleta dados inseridos pelo usuário.
     printf("Insira a população: ");
     scanf("%d", &populacao);
 
@@ -57,6 +70,8 @@ int main(){
 
     (Dpop = populacao/area);
     (PIBpc = pib/populacao);
+    (semiSp = populacao + area + (pib/1000) + Pturi);
+    (super = semiSp + Dpop + PIBpc);
 
     //impressão dos dados inseridos pelo usuário.
     printf("Estado: %s\n", estado);
@@ -64,10 +79,11 @@ int main(){
     printf("Codigo carta: %s\n", cod);
     printf("População: %d\n", populacao);
     printf("Área: %.2fkm²\n", area);
-    printf("PIB: %.2f de reais\n", pib);
+    printf("PIB: R$%.2f\n", pib);
     printf("Pontos turísticos: %d\n", Pturi);
     printf("Densidade populacional: %.2f hab/km²\n", Dpop);
-    printf("PIB per capita: %.2f reais\n", PIBpc);
+    printf("PIB per capita: %.2f Reais\n", PIBpc);
+    printf("Super: %.2f\n", super);
 
     printf("\n");
 
@@ -96,6 +112,22 @@ int main(){
     printf("Insira o codigo (Estado + número de 01 a 04): ");
     scanf("%s", cod2);
 
+        //Converte letras minúsculas para maiúsculas.
+    cod2[0] = toupper(cod2[0]);
+
+    //Compara se a primeira letra do cod é igual ao estado.
+    if (cod2[0] != estado2[0])
+    {
+        printf("Inválido");
+        return 0;
+    }
+
+    if (strcmp(cod2,cod) == 0)
+    {
+        printf("Carta repetida. INVÁlIDO!");
+        return 0;
+    }
+
     printf("Insira a população: ");
     scanf("%d", &populacao2);
 
@@ -112,6 +144,8 @@ int main(){
 
     (Dpop2 = populacao2/area2);
     (PIBpc2 = pib2/populacao2);
+    (semiSp2 = populacao2 + area2 + (pib2/1000) + Pturi2);
+    (super2 = semiSp2 + Dpop2 + PIBpc2);
 
     //impressão dos dados inseridos pelo usuário.
     printf("Estado: %s\n", estado2);
@@ -123,5 +157,114 @@ int main(){
     printf("Pontos turísticos: %d\n", Pturi2);
     printf("Densidade populacional: %.2f hab/km²\n", Dpop2);
     printf("PIB per capita: %.2f reais\n", PIBpc2);
+    printf("Super: %.2f\n", super2);
+
+    printf("\n");
+
+    printf("Comparação de cartas:\n");
+    printf("População: ");
+
+    if (populacao > populacao2)
+    {
+        printf("Carta 1 venceu (1)\n");
+        x++;
+    }
+    else
+    {
+        printf("Carta 2 venceu (0)\n");
+        y++;
+    }
+
+    printf("Àrea: ");
+
+    if (area > area2)
+    {
+        printf("Carta 1 venceu (1)\n");
+        x++;
+    }
+    else
+    {
+        printf("Carta 2 venceu (0)\n");
+        y++;
+    }
+
+        printf("PIB: ");
+
+    if (pib > pib2)
+    {
+        printf("Carta 1 venceu (1)\n");
+        x++;
+    }
+    else
+    {
+        printf("Carta 2 venceu (0)\n");
+        y++;
+    }
+
+        printf("Pontos turísticos: ");
+
+    if (Pturi > Pturi2)
+    {
+        printf("Carta 1 venceu (1)\n");
+        x++;
+    }
+    else
+    {
+        printf("Carta 2 venceu (0)\n");
+        y++;
+    }
+
+        printf("Densidade populacional: ");
+
+    if (Dpop < Dpop2)
+    {
+        printf("Carta 1 venceu (1)\n");
+        x++;
+    }
+    else
+    {
+        printf("Carta 2 venceu (0)\n");
+        y++;
+    }
+    
+        printf("PIB per capita: ");
+
+    if (PIBpc > PIBpc2)
+    {
+        printf("Carta 1 venceu (1)\n");
+        x++;
+    }
+    else
+    {
+        printf("Carta 2 venceu (0)\n");
+        y++;
+    }
+
+        printf("Super: ");
+
+    if (super > super2)
+    {
+        printf("Carta 1 venceu (1)\n");
+        x++;
+    }
+    else
+    {
+        printf("Carta 2 venceu (0)\n");
+        y++;
+    }
+
+    printf("\n");
+
+    if (x > y)
+    {
+        printf("Carta 1 venceu!");
+    }
+    else
+    {
+        printf("Carta 2 venceu!");
+    }
+    
+  
+
     return 0;
 }
